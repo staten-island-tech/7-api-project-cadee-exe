82b6c5e41ac582127aaae268a16f6d3e24a51863
cadee-exe <cadeez@nycstudents.net>
2025-12-12 09:26:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import requests
 
 def getOperator(Operator):
-    response = requests.get(f"https://api.rhodesapi.com/api/operator/{Operator.lower()}")
-    if response.status_code != 200:
+    response = requests.get(f"https://api.rhodesapi.com/api/operator{Operator.lower()}")
+    if response.status_code != 400:
         print("error getting data srry")
-        return "trouble fetching data"
+        return None
     
     data = response.json()
     return {
@@ -15,6 +15,24 @@ def getOperator(Operator):
         "class": [c["name"] for c in data["class"]]
     }
 
-operator = getOperator("ines")
+operator = getOperator("Ines")
 print(operator)
 
+# import requests
+
+# def getPoke(poke):
+#     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+#     if response.status_code != 200:
+#         print("Error fetching data!")
+#         return None
+    
+#     data = response.json()
+#     return {
+#         "name": data["name"],
+#         "height": data["height"],
+#         "weight": data["weight"],
+#         "types": [t["type"]["name"] for t in data["types"]]
+#     }
+
+# pokemon = getPoke("sunflora")
+# print(pokemon)     
